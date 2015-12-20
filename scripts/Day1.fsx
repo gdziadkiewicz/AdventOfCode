9a62997709424f2ca4415180ae97706df3d8973a
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2015-12-20 11:02:24 +0100

    Added day 1 part 2

--- scripts/Day1.fsx
+++ scripts/Day1.fsx
@@ -1,7 +1,11 @@
 ﻿let input = System.IO.File.ReadAllText "Input1.txt"
 
-let part1Answer = input |> Seq.map (fun x -> if x = '(' then 1 else -1) |> Seq.sum
+let mappedInput = input |> Seq.map (fun x -> if x = '(' then 1 else -1) 
+
+let part1Answer = mappedInput |> Seq.sum
+let part2Answer = mappedInput |> Seq.scan (+) 0 |> Seq.findIndex (fun x -> x = -1)
 
 printfn "Part1: %d" part1Answer
+printfn "Part2: %d" part2Answer
 
 System.Console.ReadKey() |> ignore 
