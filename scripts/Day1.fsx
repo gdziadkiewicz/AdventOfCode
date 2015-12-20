e10a748bb7e24b227fcad9a649bf9bbad5c2c8d9
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2015-12-20 04:15:53 +0100

    Added day 1 part 1

---
+++ scripts/Day1.fsx
@@ -0,0 +1,7 @@
+﻿let input = System.IO.File.ReadAllText "Input1.txt"
+
+let part1Answer = input |> Seq.map (fun x -> if x = '(' then 1 else -1) |> Seq.sum
+
+printfn "Part1: %d" part1Answer
+
+System.Console.ReadKey() |> ignore 
